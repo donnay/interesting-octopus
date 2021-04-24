8ebc418033aa68e925ce828920c5f183f638f661
Kevin Clarke <kevindclarke@gmail.com>
2021-04-24 16:25:45 -0700

    tweaked the Layout. to try to fix this build error

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,6 +13,11 @@ export default class Body extends React.Component {
             <React.Fragment>
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.seo.title', null) ? (_.get(this.props, 'pageContext.frontmatter.seo.title', null)) : _.get(this.props, 'pageContext.frontmatter.title', null) + ' | ' + _.get(this.props, 'pageContext.site.siteMetadata.title', null)}</title>
+                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+<link rel="manifest" href="/images/favicon/site.webmanifest"></link>
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.seo.description', null) || ''} />
@@ -41,13 +46,6 @@ export default class Body extends React.Component {
                     <link rel="icon" href={withPrefix(_.get(this.props, 'pageContext.site.siteMetadata.favicon', null))}/>
                     )}
                     <body className={'palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette', null)} />
-                    
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
-<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
-<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
-<link rel="manifest" href="/images/favicon/site.webmanifest"></link>
-
                 </Helmet>
                 <div id="page" className="site">
                   <Header {...this.props} />
