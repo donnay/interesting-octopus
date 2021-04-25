290d94181e65909943697749ca9b75847e7e4d44
Kevin Clarke <kevindclarke@gmail.com>
2021-04-24 19:16:56 -0700

    tweaked

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,7 +17,8 @@ export default class Body extends React.Component {
 <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
-<link rel="manifest" href="/images/favicon/site.webmanifest"></link>
+<link rel="manifest" href="/images/favicon/site.webmanifest">
+</link>
                     <meta charSet="utf-8"/>
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.seo.description', null) || ''} />
